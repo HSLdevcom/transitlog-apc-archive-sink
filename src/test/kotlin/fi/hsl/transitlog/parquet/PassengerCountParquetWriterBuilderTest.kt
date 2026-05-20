a42687b4ef03c2104b49c641355f13decfe45c0f
Anton Jyrkiäinen <anton.jyrkiainen@helsinki.fi>
2026-05-20 14:31:42 +0300

    add missing import

--- src/test/kotlin/fi/hsl/transitlog/parquet/PassengerCountParquetWriterBuilderTest.kt
+++ src/test/kotlin/fi/hsl/transitlog/parquet/PassengerCountParquetWriterBuilderTest.kt
@@ -8,6 +8,7 @@ import java.nio.file.Path
 import java.time.Instant
 import kotlin.test.Test
 import kotlin.test.assertTrue
+import org.junit.jupiter.api.Disabled
 
 @Disabled("Hadoop/Parquet currently incompatible with Java 25 Subject.getSubject")
 class PassengerCountParquetWriterBuilderTest {
